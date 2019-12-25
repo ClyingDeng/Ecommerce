484846f2d4975d367972096c2e041a1dcbd0d35f
ClyingDeng <1174915260@qq.com>
2019-12-25 17:52:47 +0800

    获取用户token

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,17 +1,20 @@
-import { getSetting, chooseAddress, openSetting } from '../../utils/asyncWX.js'
+import { getSetting, chooseAddress, openSetting, showModal, showToast } from '../../utils/asyncWX.js'
 const regeneratorRuntime = require('../../lib/runtime.js');
 Page({
   data: {
     address: {},
-    carts: []
+    carts: [],
+    allChecked: false,
+    totalPrice: 0,
+    totalNum: 0,
   },
   onShow() {
     //获取本地地址数据
     const address = wx.getStorageSync('address');
     //获取缓存cart数据
-    const carts = wx.getStorageSync('cart');
-    //给data赋值
-    this.setData({ address, carts });
+    const carts = wx.getStorageSync('cart') || [];
+    this.setData({ address });
+    this.setCart(carts);
 
   },
   onload() {
@@ -64,7 +67,7 @@ Page({
       //收货地址存入缓存
       wx.setStorageSync('address', address);
 
-      console.log(address);
+
 
 
 
@@ -73,14 +76,85 @@ Page({
     }
 
   },
-  handleNum(e) {
+  async handleNum(e) {
     // 获取传递过来的参数
     const { opration, id } = e.currentTarget.dataset;
     console.log(opration, id);
-    const {cart} = this.data;
-    const index = cart.findIndex(v=>v.goods_id === id);
-    cart[index].handleNum += opration;
-    this.setCart(cart);
-  }
+    let { carts } = this.data;
+    const index = carts.findIndex(v => v.goods_id === id);
+    //判断是否要执行删除
+    if (carts[index].num === 1 && opration === -1) {
+      //弹窗提示
+      const res = await showModal({ content: '您是否要删除？' });
+      if (res.confirm) {
+        carts.splice(index, 1);
+        this.setCart(carts);
 
+      }
+    } else {
+      carts[index].num += opration;
+      console.log({ carts });
+      this.setCart(carts);
+    }
+  },
+  //商品选中
+  handleItemChange(e) {
+    //获取被修改的商品的id
+    const goods_id = e.currentTarget.dataset.id;
+    //获取购物车数组
+    let { carts } = this.data;
+    //找到被修改的商品对象
+    let index = carts.findIndex(v => v.goods_id === goods_id);
+    //选中状态取反
+    carts[index].checked = !carts[index].checked;
+    //5 6把购物车数据重新设置回data中和缓存中
+    this.setCart(carts);
+
+  },
+  //商品的全选功能
+  handleAllChk() {
+    let { carts, allChecked } = this.data;
+    allChecked = !allChecked;
+    carts.forEach(v => v.checked = allChecked);
+    console.log(carts);
+    this.setCart(carts);
+  },
+  //设置购物车状态同时，重新计算 底部工具栏的数据 全选 总价格 购买数量
+  setCart(carts) {
+    let allChecked = true;
+    //总价格 总数量
+    let totalPrice = 0;
+    let totalNum = 0;
+    carts.forEach(v => {
+      if (v.checked) {
+        totalPrice += v.num * v.goods_price;
+        totalNum += v.num;
+      } else {
+        allChecked = false;
+      }
+    });
+    allChecked = carts.length ? allChecked : false;
+    //给data赋值
+    this.setData({ carts, allChecked, totalNum, totalPrice });
+    wx.setStorageSync('cart', carts);
+  },
+  //支付
+  async handlePay() {
+    //判断收货地址
+    const { address, totalNum } = this.data;
+    if (!address.userName) {
+      await showToast({ title: "您还没有选择收货地址" })
+      return;
+    }
+    //判断有没有选购
+    if (totalNum == 0) {
+      await showToast({ title: "您还没有选购商品" })
+      return;
+    }
+    //跳转到支付页面
+    wx.navigateTo({
+      url: '/pages/pay/index'
+    });
+
+  }
 })
--- utils/asyncWX.js
+++ utils/asyncWX.js
@@ -35,3 +35,56 @@ export const openSetting = () => {
         });
     })
 }
+/**
+ * promise 形式 showModal
+ * @params {object} param0 参数
+ */
+export const showModal = ({ content }) => {
+    return new Promise((resolve, reject) => {
+        //弹窗提示
+        wx.showModal({
+            title: '提示',
+            content: content,
+            success: (res) => {
+                resolve(res);
+            },
+            fail: (err) => {
+                reject(err);
+            }
+
+        })
+    })
+}
+
+export const showToast = ({ title }) => {
+    return new Promise((resolve, reject) => {
+        //弹窗提示
+        wx.showToast({
+            title: title,
+            icon: 'none',
+            success: (res) => {
+                resolve(res);
+            },
+            fail: (err) => {
+                reject(err);
+            }
+
+        })
+    })
+}
+//登录
+export const login = () => {
+    return new Promise((resolve, reject) => {
+        wx.login({
+            timeout:10000,
+            success: (res) => {
+                resolve(res);
+            },
+            fail: (err) => {
+                reject(err);
+            }
+        });
+          
+        
+    })
+}
